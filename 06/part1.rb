6e2d4342acdf4cd8c00dd14865a0c68dd4158138
Alexandre Ignjatovic <alexandre.ignjatovic@gmail.com>
2022-12-06 14:02:49 +0100

    Day 6

---
+++ 06/part1.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+require_relative '../assertion'
+
+def perform(input)
+  buffer = input.chomp
+  (3...input.size).each do |index|
+    packs_of_four = buffer[(index - 3)..index]
+    return index + 1 if packs_of_four.chars.uniq.size == 4
+  end
+end
+
+should_return(7).for { perform('mjqjpqmgbljsphdztnvjfqwrcgsmlb') }
+# bvwbjplbgvbhsrlpgdmjqwftvncz: first marker after character 5
+should_return(5).for { perform('bvwbjplbgvbhsrlpgdmjqwftvncz') }
+# nppdvjthqldpwncqszvftbrmjlhg: first marker after character 6
+should_return(6).for { perform('nppdvjthqldpwncqszvftbrmjlhg') }
+# nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg: first marker after character 10
+should_return(10).for { perform('nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg') }
+# zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw: first marker after character 11
+should_return(11).for { perform('zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw') }
+
+puts perform(File.read('input.txt'))
